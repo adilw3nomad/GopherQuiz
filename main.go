af87940ef0f0be50edd369d2705a3c0df55d8a01
adilw3nomad <adiln1993@googlemail.com>
2019-06-04 04:04:11 +0100

    Refactoring, adding test for check answer, reformatting

--- main.go
+++ main.go
@@ -36,25 +36,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for i := 0; i < len(records); i++ {
-		// Create quizItem object
+	for i, _ := range records {
+		// Create a quizItem object.
 		quizItem := quizItem{records[i][0], records[i][1]}
-		// Print out the question
+
+		// Print out the question to the user.
 		fmt.Println("Question:", quizItem.question)
-		// Create reader and allow user to input their answer
+
+		// Create a reader and allow the user to input their answer.
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter your answer now: ")
-		// Expect answer to be given once they hit return
-		text, _ := reader.ReadString('\n')
+
+		// Expect an answer to be given once they hit return.
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		fmt.Println("Your answer is:", text)
+
 		// Trim the newline suffix from the input
 		text = strings.TrimSuffix(text, "\n")
-		if text == quizItem.answer {
-			fmt.Println("Correct! Well done")
-		} else {
-			fmt.Println("WRONG! Answer is:", quizItem.answer)
-		}
+
+		fmt.Println(CheckAnswer(text, quizItem.answer))
 	}
 
 }
